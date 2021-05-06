235f2b69db5f8a6d22a40f30e7fd234a0deef8ca
Hamzah <64706364+hamzah2412@users.noreply.github.com>
2021-05-06 16:48:37 +0800

    add "" on db.json and fetching data from json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './Components/Header'
 import Tasks from './Components/Tasks'
 import AddTask from './Components/AddTask'
@@ -7,26 +7,18 @@ import AddTask from './Components/AddTask'
 const App = () => {
     const [showAddTask, setShowAddTask] = useState(false)
 
-    const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    },      
-  ])
+    const [tasks, setTasks] = useState([])
+
+useEffect(() => {
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    
+    console.log(data)
+  }
+
+  fetchTasks()
+}, [])
 
 //Add a new Task
 const addTask = (task) =>{
